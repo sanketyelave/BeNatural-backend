99ec73f5c5c3438456377122736c0b9b8d065f7e
Sanket Y <124805576+sanketyelave@users.noreply.github.com>
2023-12-17 14:24:20 +0530

    Update 3 index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const passport = require('passport');
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin:  'https://657ea9771ade9141144729ff--magenta-dusk-440146.netlify.app',
+    origin:  'https://magenta-dusk-440146.netlify.app/',
     credentials: true,
 };
 
